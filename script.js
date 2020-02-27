575d225dbebc3cee4a3de4b3c4c1ed9c71b7f501
fahadlatif816 <fahadlatif816@gmail.com>
2020-02-27 16:14:47 +0500

    Completed  the TASK.

---
+++ script.js
@@ -0,0 +1,164 @@
+let pendingList=[];
+let completedList=[];
+
+if(pendingList.length==0)
+{
+    $("#demoPending").html("To do List of Pending Tasks is Empty!!");
+}
+
+if(completedList.length==0)
+{
+    $("#demoCompleted").html("To do List of Completd Tasks is Empty!!");
+}
+$( "#PList" ).sortable();
+$( "#PList" ).disableSelection();
+$( "#TDList" ).sortable();
+$( "#TDList" ).disableSelection();
+
+
+let input = document.getElementById("todoText");
+input.addEventListener("keyup", function(event) {
+if (event.keyCode === 13) {
+    addItem();
+}
+});
+
+function addItem(){
+    regenerateList();
+    let text = $("#todoText").val();
+    if(text!=null && text!="")
+    {
+        if(pendingList.indexOf(text) ==-1 && completedList.indexOf(text) ==-1)
+        {
+            pendingList.push(text);
+            populatePendingList();
+            $("#todoText").val("");
+            $("#todoText").focus();
+        }
+        else
+        {
+            alert("Can't Repeat the same To-do Again!!");
+        }
+        
+    }
+    else
+    {
+        alert("To-do List Can't be Empty!!");
+    }
+}
+
+function addtoCompleted(obj)
+{
+    regenerateList();
+    completedList.push(pendingList[obj.id]);
+    pendingList.splice(obj.id,1);
+    $("#demoCompleted").html("");
+    populatePendingList();
+    populateCompletedList();
+}
+
+function deletePending (obj)
+{
+    pendingList.splice(obj.id,1);
+    populatePendingList();
+}
+
+function deleteCompleted (obj)
+{
+    completedList.splice(obj.id,1);
+    populateCompletedList();
+}
+
+function editPending(obj)
+{
+    console.log(pendingList);
+    let value = pendingList[obj.id];
+    let text = prompt("Please edit the list name:", value);
+    if (text == null || text == "") {
+        alert("To-do List Can't be Empty!!");
+    } else {
+        delete pendingList[obj.id];
+        if(pendingList.indexOf(text) ==-1 && completedList.indexOf(text) ==-1 && text.length<=25)
+        {
+            pendingList[obj.id]=text;
+            populatePendingList();
+            $("#todoText").val("");
+            $("#todoText").focus();
+        }
+        else
+        {
+            pendingList[obj.id]=value;
+            if(text.length>25)
+            {
+                alert("Size shouldn't be exceed by 25 characters.")
+            }
+            else
+            {
+                alert("Cant Repeat the same To-do Again!!");
+            }
+        }
+    }
+    console.log(pendingList);
+}
+
+function regenerateList()
+{
+    let allToDoListElements = $("li.pendingLI span.listContent");
+    let allCompletedListElements = $("li.todoLI span.listContent");
+    let allPendingDelBtn = $("li.pendingLI span.cross button.crossButton");
+    let allpendingEditBtn = $("li.pendingLI span.cross button.editButton");
+    let allToDoDelBtn = $("li.todoLI span.cross button.crossButton");
+
+    for(let i=0; i < allToDoListElements.length;i++)
+    {
+        pendingList[i] = allToDoListElements[i].innerText;
+        allToDoListElements[i].id=i+"";
+        allPendingDelBtn[i].id=i+"";
+        allpendingEditBtn[i].id=i+"";
+    }
+
+    for(let i=0; i < allCompletedListElements.length;i++)
+    {
+        completedList[i] = allCompletedListElements[i].innerText;
+        allCompletedListElements[i].id=i+"";
+        allToDoDelBtn[i].id=i+"";
+    }
+}
+
+function populatePendingList()
+{
+    $("#PList").html("");
+    $("#PList").append(`<h3>Pending Tasks List</h3>
+    <p id="demoPending"></p>`);
+    for(let i=0; i<pendingList.length;i++)
+    {
+        $("#PList").append(`<li class="pendingLI" onmouseover="regenerateList()">
+                <span class="listContent" onclick="addtoCompleted(this)" id=${i} >${pendingList[i]}</span>
+                <span class="cross"><button class="crossButton" id=${i} onclick="editPending(this)">E</button>
+                <button class="editButton" id=${i} onclick="deletePending(this)">D</button>
+                </span>
+                </li>`);
+    }
+    if(pendingList.length==0)
+    {
+        $("#demoPending").html("To do List of Pending Tasks is Empty!!");
+    }
+}
+
+function populateCompletedList()
+{
+    $("#TDList").html("");
+    $("#TDList").append(`<h3>Completed Tasks List</h3>
+    <p id="demoCompleted"></p>`);
+    for(let i=0; i<completedList.length;i++)
+    {
+        $("#TDList").append(`<li class="todoLI" onmouseover="regenerateList()">
+                <span class="listContent">${completedList[i]}</span>
+                <span class="cross"><button class="crossButton" id=${i} onclick="deleteCompleted(this)">D</button></span>
+                </li>`);
+    }
+    if(completedList.length==0)
+    {
+        $("#demoCompleted").html("To do List of Completd Tasks is Empty!!");
+    }
+}
